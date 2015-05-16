4a180645533cd609be797297d5f2dcbf49d12792
Brett Cannon <brett@python.org>
2015-05-16 10:18:44 -0400

    Parallelize repo creation

--- freelabour/conf.py
+++ freelabour/conf.py
@@ -21,38 +21,52 @@ def repo_type(domain_mapping, url):
     raise ValueError('repo type of {!r} is unknown'.format(domain))
 
 
+def _urls(repository):
+    urls = repository.get('urls')
+    if urls is None:
+        urls = [repository['url']]
+    yield from urls
+
+
+def _repos(repository, repo_mapping):
+    for url in _urls(repository):
+        yield repo_mapping[url]
+
+
 def process(data, dest_parent):
     future_projects = set()
-    pool = futures.ThreadPoolExecutor(os.cpu_count() or 1)
     user_name = data['name']
     domain_mapping = data['VCS type by domain']
-    # TODO: make concurrent; probably name of project with repo and then
-    # groupby after fetching them all. Can keep creating projects concurrently.
-    for repository in data['repositories']:
-        repos = []
-        urls = repository.get('urls')
-        if urls is None:
-            urls = [repository['url']]
-        for url in urls:
-            type_ = repository.get('type')
-            if type_ is None:
-                type_ = repo_type(domain_mapping, url)
-            try:
-                repos.append(repo.Repo.get(type_, url, dest_parent))
-                print(url)
-            except ValueError as exc:
-                print('{}: {}'.format(url, str(exc)))
-                continue
-        if not repos:
-            continue
-        claimed_commits = repository.get('commits', [])
-        for claimed_commit in claimed_commits:
-            for r in repos:
+    with futures.ThreadPoolExecutor(os.cpu_count() or 1) as pool:
+        # Clone/update all repos.
+        future_repos = set()
+        for repository in data['repositories']:
+            for url in _urls(repository):
+                type_ = repository.get('type')
+                if type_ is None:
+                    type_ = repo_type(domain_mapping, url)
                 try:
-                    r.claim_commit(user_name, claimed_commit)
-                except ValueError:
-                    pass
-        f = pool.submit(project.Project, data['name'],
-                        repository.get('name'), *repos)
-        future_projects.add(f)
-    return frozenset(f.result() for f in future_projects)
+                    f = pool.submit(repo.Repo.get, type_, url, dest_parent)
+                    future_repos.add(f)
+                    print(url)
+                except ValueError as exc:
+                    print('{}: {}'.format(url, str(exc)))
+                    continue
+        repo_mapping = {future_repo.result().remote: future_repo.result()
+                        for future_repo in future_repos}
+        # Handle claimed commits.
+        for repository in data['repositories']:
+            claimed_commits = repository.get('commits', [])
+            for claimed_commit in claimed_commits:
+                for r in _repos(repository, repo_mapping):
+                    try:
+                        r.claim_commit(user_name, claimed_commit)
+                    except ValueError:
+                        pass
+        # Create projects.
+        for repository in data['repositories']:
+            f = pool.submit(project.Project, data['name'],
+                            repository.get('name'),
+                            *_repos(repository, repo_mapping))
+            future_projects.add(f)
+        return frozenset(f.result() for f in future_projects)
